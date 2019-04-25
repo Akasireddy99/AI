37c0184f50a13a3c7bad6ba33d6f64ffc3aa1fda
Abhishek Kasireddy <Akasireddy99@gmail.com>
2019-04-25 14:44:55 +0530

    Removed duplicates

--- graphcolor.py
+++ graphcolor.py
@@ -15,23 +15,6 @@ def promising(state, color):
         color_of_neighbor = colors_of_states.get(neighbor)
         if color_of_neighbor == color:
             return False
-colors = ['Red', 'Blue', 'Green', 'Yellow', 'Black']
-
-states = ['Andhra', 'Karnataka', 'TamilNadu', 'Kerala']
-
-neighbors = {}
-neighbors['Andhra'] = ['Karnataka', 'TamilNadu']
-neighbors['Karnataka'] = ['Andhra', 'TamilNadu', 'Kerala']
-neighbors['TamilNadu'] = ['Andhra', 'Karnataka', 'Kerala']
-neighbors['Kerala'] = ['Karnataka', 'TamilNadu']
-
-colors_of_states = {}
-
-def promising(state, color):
-    for neighbor in neighbors.get(state):
-        color_of_neighbor = colors_of_states.get(neighbor)
-        if color_of_neighbor == color:
-            return False
 
     return True
 
